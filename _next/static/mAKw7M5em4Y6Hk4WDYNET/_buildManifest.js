944b4fdbdba6f41a80063009458e4f02ee811fd0
Daniel Quinn <danquinndev@gmail.com>
2021-08-14 22:11:07 -0700

    Update

---
+++ _next/static/mAKw7M5em4Y6Hk4WDYNET/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-26e50a85a3acc81850fb.js"],"/_error":["static/chunks/pages/_error-eb7f79f2af83e8ddf5e2.js"],"/posts/[id]":["static/chunks/pages/posts/[id]-191e23b204acc0472899.js"],sortedPages:["/","/_app","/_error","/posts/[id]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
